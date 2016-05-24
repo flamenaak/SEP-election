0b6c25d60d3592fa6232d43da7a184d3a413a028
BenSolyom <solyombence4445@gmail.com>
2016-05-24 21:24:07 +0200

    Controller
    Controller and IController class

---
+++ src/controller/IController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import model.Candidate;
+import model.Election;
+import model.Position;
+
+
+public interface IController {
+	
+	public void logIn(String name, String password);
+	public void logOut();
+	public boolean checkPassword(String password);
+	public boolean checkName(String name);
+	
+	public void changePassword(String password);
+
+	public void createElection();
+	public void startElection(Election election);
+	public void endElection(Election election);
+	public void addPosition(Election election, Position position);
+	public void addCandidate(Election election, Position position, Candidate candidate);
+	
+	public void viewCandidatesAndPositions();
+	public void viewResults(Election election, Position position);
+}
