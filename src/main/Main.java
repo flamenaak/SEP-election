ab5023ae222d405da04e6c35c8128ee05cf795de
Vladimir Rocin <vlada.rocin@gmail.com>
2016-05-25 21:59:34 +0200

    main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,12 @@
 package main;
 
-<<<<<<< HEAD
 import controller.Controller;
-=======
+
 import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
->>>>>>> refs/remotes/origin/Ben
 import model.Candidate;
 import model.Election;
 import model.Position;
@@ -20,31 +18,30 @@ import view.VoterWindow;
 import javax.swing.JFrame;
 
 import view.NewPositionPanel;
+
 public class Main
 {
 
    public static void main(String[] args)
-<<<<<<< HEAD
-   { 
-     VoterWindow vw = new VoterWindow();
-     vw.setComponents();
+   {
+      VoterWindow vw = new VoterWindow();
+      vw.setComponents();
 
-    // VoterWindow vw = new VoterWindow();
-     //vw.setComponents();
-      //AdminWindow win = new AdminWindow();
+      // VoterWindow vw = new VoterWindow();
+      // vw.setComponents();
+      // AdminWindow win = new AdminWindow();
       JFrame frame = new JFrame();
       NewPositionPanel pan = new NewPositionPanel();
-        //ResultsView pan = new ResultsView();   
+      // ResultsView pan = new ResultsView();
       frame.add(pan);
-      frame.setVisible(true); 
-=======
-   {
-	 //LogInWindow lw = new LogInWindow();
-	 AdminWindow aw = new AdminWindow();
-     //VoterWindow vw = new VoterWindow();
+      frame.setVisible(true);
+      {
+         // LogInWindow lw = new LogInWindow();
+         AdminWindow aw = new AdminWindow();
+         // VoterWindow vw = new VoterWindow();
 
-     //vw.setComponents();
->>>>>>> refs/remotes/origin/Ben
+         // vw.setComponents();
+      }
+
    }
-
 }
