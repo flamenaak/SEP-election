d201f9a4852c149b6caf8610dd9f8665fcf4ff09
Vladimir Rocin <vlada.rocin@gmail.com>
2016-05-25 21:59:51 +0200

    addMain

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,47 @@
 package main;
 
+import controller.Controller;
+
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+
+import model.Candidate;
+import model.Election;
+import model.Position;
+import view.AdminWindow;
+import view.LogInWindow;
+import view.NewCandidatePanel;
+import view.VoterWindow;
+
+import javax.swing.JFrame;
+
+import view.NewPositionPanel;
+
 public class Main
 {
 
+   public static void main(String[] args)
+   {
+      VoterWindow vw = new VoterWindow();
+      vw.setComponents();
+
+      // VoterWindow vw = new VoterWindow();
+      // vw.setComponents();
+      // AdminWindow win = new AdminWindow();
+      JFrame frame = new JFrame();
+      NewPositionPanel pan = new NewPositionPanel();
+      // ResultsView pan = new ResultsView();
+      frame.add(pan);
+      frame.setVisible(true);
+      {
+         // LogInWindow lw = new LogInWindow();
+         AdminWindow aw = new AdminWindow();
+         // VoterWindow vw = new VoterWindow();
+
+         // vw.setComponents();
+      }
+
+   }
 }
