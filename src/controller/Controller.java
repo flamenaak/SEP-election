8a65834eb9e5f6f002330fa64959247c88d0d7a4
Vladimir Rocin <vlada.rocin@gmail.com>
2016-05-25 15:25:38 +0200

    MinorCont fix, expect more

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -12,97 +12,85 @@ import model.Voter;
 
 public class Controller implements IController{
 
-	private User user;
-	private Voter voter;
-	private Admin admin;
-	private Election election;
-	private DBManager dbm;
-	private Position position;
-	
-	public Controller() {
-		user = new User();
-		voter = new Voter();
-		admin = new Admin();
-		election = new Election();
-	}
-	
-	public Controller(User user) {
-		this.user = user;
-	}
-	
-	public Controller(Admin admin) {
-		this.admin = admin;
-	}
-	
-	public Controller(Voter voter) {
-		this.voter = voter;
-	}
-	
-	@Override
-	public void logIn(String name, String password) {
-		
-	}
+   private User user;
+   private Voter voter;
+   private Admin admin;
+   private Election election;
+   private DBManager dbm;
+   
+   public Controller() {
+      user = new User();
+      voter = new Voter();
+      admin = new Admin();
+      election = new Election();
+   }
+   
+   
+   @Override
+   public void logIn(String name, String password) {
+      
+   }
 
-	@Override
-	public void logOut() {
-		
-	}
-	
-	public boolean checkName(String name) {
-		return name.equals(user.getName());
-	}
+   @Override
+   public void logOut() {
+      
+   }
+   
+   public boolean checkName(String name) {
+      return name.equals(user.getName());
+   }
 
-	@Override
-	public boolean checkPassword(String password) {
-		return password.equals(user.getPassword());
-	}
+   @Override
+   public boolean checkPassword(String password) {
+      return password.equals(user.getPassword());
+   }
 
-	@Override
-	public void changePassword(String password) {
-		admin.setPassword(password);
-	}
+   @Override
+   public void changePassword(String password) {
+      admin.setPassword(password);
+   }
 
-	@Override
-	public void createElection() {
-		
-	}
+   @Override
+   public void createElection() {
+      
+   }
 
-	@Override
-	public void startElection(Election election) {
-		election.start();
-	}
-	
-	@Override
-	public void endElection(Election election) {
-		election.end();
-	}
+   @Override
+   public void startElection(Election election) {
+      election.start();
+   }
+   
+   @Override
+   public void endElection(Election election) {
+      election.end();
+   }
 
-	@Override
-	public void addPosition(Election election, Position position) {
-		election.addPosition(position);
-	}
+   @Override
+   public void addPosition(Election election, Position position) {
+      election.getPosition(position);
+   }
 
-	@Override
-	public void addCandidate(Election election, Position position, Candidate candidate) {
-		election.getPosition(position).addCandidate(candidate);
-	}
+   @Override
+   public void addCandidate(Election election, Position position, Candidate candidate) {
+      election.getPosition(position).addCandidate(candidate);
+   }
 
-	@Override
-	public ArrayList<Candidate> viewCandidatesAndPositions() {
-		ArrayList<Candidate> candidates = new ArrayList<>();
+   @Override
+   public ArrayList<Candidate> viewCandidatesAndPositions() {
 
-		for (int i = 0; i < election.getPositions.size(); i++) {
-			for (int j = 0; j < array.length; j++) {
-				candidates.add(election.getPosition(i).getCandidate(j));
-			}
-		}
-		
-		return candidates;
-	}
+      return election.getAllCandidates();;
+   }
 
-	public void viewResults(Position position) {
+   @Override
+   public ArrayList<Candidate> viewResults(Election election, Position position) {
+      ArrayList<Candidate> winning = new ArrayList<>();
 
-	}
+      for (int i = 0; i < election.getPositionSize(); i++) {
+         winning.add(election.getPosition(i).getWinningCandidate());
+      }
+      
+      return winning;
+   }
 
 
    @Override
