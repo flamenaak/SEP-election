bc27296f022b894aadaa9b7485dbf694bd53873c
Doms61 <dominik.lukac.96@gmail.com>
2016-05-26 22:15:14 +0200

    new Branch

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,6 @@
 package main;
-
 import controller.Controller;
-import java.awt.Font;
-import javax.swing.JFrame;
-import javax.swing.JTabbedPane;
-import model.Candidate;
-import model.Election;
-import model.Position;
 import view.AdminWindow;
-import view.LogInWindow;
-import view.NewCandidatePanel;
-import view.VoterWindow;
-import javax.swing.JFrame;
-import view.NewPositionPanel;
 
 public class Main
 {
