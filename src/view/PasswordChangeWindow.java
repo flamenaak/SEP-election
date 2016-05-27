c5b458c15c91b24569599e04a5895c9c8d195002
Vladimir Rocin <vlada.rocin@gmail.com>
2016-05-27 16:20:09 +0200

    PassWord change window

--- src/view/PasswordChangeWindow.java
+++ src/view/PasswordChangeWindow.java
@@ -3,6 +3,8 @@ package view;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -12,47 +14,59 @@ import javax.swing.JPasswordField;
 import controller.Controller;
 
 public class PasswordChangeWindow extends JPanel{
-	private JPasswordField passwordField;
-	private JPasswordField passwordField_1;
-	private GridBagConstraints c;
-	private Controller cont;
+   private JPasswordField passwordField;
+   private JPasswordField passwordField_1;
+   private GridBagConstraints c;
+   private Controller controller;
 
-	public PasswordChangeWindow(Controller cont) {
-	super(new GridBagLayout());
-	this.cont = cont;
-	c = new GridBagConstraints();
-	
-	JLabel lblNewLabel = new JLabel("Enter new password");
-	c.gridx = 0;
-	c.gridy = 0;
-	add(lblNewLabel, c);
-	
-	JLabel lblNewLabel_1 = new JLabel("Confirm new password");
-	c.gridy = 1;
-	add(lblNewLabel_1, c);
-	
-	passwordField = new JPasswordField();
-	passwordField.setPreferredSize(new Dimension(130, 22));
-	c.gridx = 1;
-	c.gridy = 0;
-	add(passwordField);
-	
-	passwordField_1 = new JPasswordField();
-   passwordField_1.setPreferredSize(new Dimension(130, 22));
-   c.gridx = 1;
-   c.gridy = 1;
-   add(passwordField_1, c);
-	
+   public PasswordChangeWindow(Controller controller) {
+      super(new GridBagLayout());
+      this.controller = controller;
+      c = new GridBagConstraints();
+
+      JLabel lblNewLabel = new JLabel("Enter new password");
+      c.gridx = 0;
+      c.gridy = 0;
+      add(lblNewLabel, c);
+
+      JLabel lblNewLabel_1 = new JLabel("Confirm new password");
+      c.gridy = 1;
+      add(lblNewLabel_1, c);
+
+      passwordField = new JPasswordField();
+      passwordField.setPreferredSize(new Dimension(130, 22));
+      c.gridx = 1;
+      c.gridy = 0;
+      add(passwordField);
+
+      passwordField_1 = new JPasswordField();
+      passwordField_1.setPreferredSize(new Dimension(130, 22));
+      c.gridx = 1;
+      c.gridy = 1;
+      add(passwordField_1, c);
+
+
+      JButton btnSavePassword = new JButton("Save password");
+
+      if (passwordField.getPassword().equals("") || passwordField_1.getPassword().equals("") || passwordField.getPassword().equals(passwordField_1.getPassword()))
+         btnSavePassword.setEnabled(false);
+
+      btnSavePassword.setBounds(227, 168, 119, 58);
+      c.gridx = 0;
+      c.gridy = 3;
+      add(btnSavePassword, c);
+   }
    
-	JButton btnSavePassword = new JButton("Save password");
-	
-	if (passwordField.getPassword().equals("") || passwordField_1.getPassword().equals("") || passwordField.getPassword().equals(passwordField_1.getPassword()))
-		btnSavePassword.setEnabled(false);
-		
-	btnSavePassword.setBounds(227, 168, 119, 58);
-	c.gridx = 0;
-   c.gridy = 3;
-	add(btnSavePassword, c);
-	}
-	
+   public class buttonListener implements ActionListener {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+         String password = "";
+         char[] chars = passwordField.getPassword();
+         for (int i = 0; i < passwordField.getPassword().length; i++) {
+            password += chars[i];
+         }
+         controller.changePassword(password);
+      }  
+   }
+
 }
