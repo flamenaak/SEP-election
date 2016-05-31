c2a55dd6d36e040cabb1b3779dc132c379d7af14
Doms61 <dominik.lukac.96@gmail.com>
2016-05-31 18:17:38 +0200

    changeButton

--- src/view/LogInWindow.java
+++ src/view/LogInWindow.java
@@ -7,6 +7,8 @@ import java.awt.TextField;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
--- src/view/PasswordChangeWindow.java
+++ src/view/PasswordChangeWindow.java
@@ -5,6 +5,9 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Arrays;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -13,13 +16,16 @@ import javax.swing.JPasswordField;
 
 import controller.Controller;
 
-public class PasswordChangeWindow extends JPanel{
+public class PasswordChangeWindow extends JPanel implements KeyListener
+{
    private JPasswordField passwordField;
    private JPasswordField passwordField_1;
    private GridBagConstraints c;
    private Controller controller;
+   private JButton btnSavePassword;
 
-   public PasswordChangeWindow(Controller controller) {
+   public PasswordChangeWindow(Controller controller)
+   {
       super(new GridBagLayout());
       this.controller = controller;
       c = new GridBagConstraints();
@@ -38,40 +44,67 @@ public class PasswordChangeWindow extends JPanel{
       c.gridx = 1;
       c.gridy = 0;
       add(passwordField);
+      passwordField.addKeyListener(this);
 
       passwordField_1 = new JPasswordField();
       passwordField_1.setPreferredSize(new Dimension(130, 22));
       c.gridx = 1;
       c.gridy = 1;
       add(passwordField_1, c);
+      passwordField_1.addKeyListener(this);
 
-
-      JButton btnSavePassword = new JButton("Save password");
+      btnSavePassword = new JButton("Save password");
 
       btnSavePassword.addActionListener(new buttonListener());
-      
-      if (passwordField.getPassword().equals("") || passwordField_1.getPassword().equals("") || passwordField.getPassword().equals(passwordField_1.getPassword()))
-         btnSavePassword.setEnabled(false);
 
       btnSavePassword.setBounds(227, 168, 119, 58);
       c.gridx = 0;
       c.gridy = 3;
       add(btnSavePassword, c);
-      
+
    }
-   
-   public class buttonListener implements ActionListener {
+
+   public class buttonListener implements ActionListener
+   {
       @Override
-      public void actionPerformed(ActionEvent e) {
+      public void actionPerformed(ActionEvent e)
+      {
          String password = "";
          char[] chars = passwordField.getPassword();
-         for (int i = 0; i < passwordField.getPassword().length; i++) {
+         for (int i = 0; i < passwordField.getPassword().length; i++)
+         {
             password += chars[i];
          }
          controller.changePassword(password);
          passwordField.setText("");
          passwordField_1.setText("");
-      }  
+      }
+   }
+
+   @Override
+   public void keyPressed(KeyEvent arg0)
+   {
+   }
+
+   @Override
+   public void keyReleased(KeyEvent arg0)
+   {
+
+      char[] pass = passwordField.getPassword();
+      char[] pass2 = passwordField_1.getPassword();
+      if (Arrays.equals(pass, pass2))
+      {
+         btnSavePassword.setEnabled(true);
+      }
+      else
+      {
+         btnSavePassword.setEnabled(false);
+      }
+   }
+
+   @Override
+   public void keyTyped(KeyEvent arg0)
+   {
    }
 
 }
